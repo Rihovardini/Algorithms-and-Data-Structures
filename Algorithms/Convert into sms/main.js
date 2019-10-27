8be54e9ae5aa530940af75d0f43fbd7370d16ecc
Ihor Andriiv <andriivihor@icloud.com>
2019-10-27 18:49:11 +0200

    Update main.js

--- Algorithms/Convert into sms/main.js
+++ Algorithms/Convert into sms/main.js
@@ -1,8 +1,8 @@
   function solutionMessages(string, maxSmsLength) {
     const words = string.split(' ');
     const isValid = words.some(word => word.length <= maxSmsLength) 
-                    || string.length <= 500
-                    || string.lastELement > 0;
+                    && string.length <= 500
+                    && string.lastELement > 0;
 
     if(!isValid) {
       return -1;
