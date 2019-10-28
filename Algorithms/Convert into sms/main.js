1659270cf440b522b82cbb1b5adb9ee37570084e
Ihor Andriiv <andriivihor@icloud.com>
2019-10-28 18:30:40 +0200

    Update main.js

--- Algorithms/Convert into sms/main.js
+++ Algorithms/Convert into sms/main.js
@@ -2,26 +2,17 @@
     const words = string.split(' ');
     const isValid = words.some(word => word.length <= maxSmsLength) 
                     && string.length <= 500
-                    && string.lastELement > 0;
+                    && string.length > 0;
 
     if(!isValid) {
       return -1;
     }
 
     return words.reduce((acc, word) => {
-      return getSmsArray([...acc], word, maxSmsLength);
+          const lastELement = acc.pop();
+          const newSms = `${lastELement} ${word}`.trim();
+          return newSms.length > maxSmsLength
+                         ? [...acc, lastELement, word]
+                         : [...acc, newSms];
     }, ['']).length;
-  }
-
-  function getSmsArray(smsArray, word, maxSmsLength) {
-    const lastIndex = smsArray.length - 1;
-    const lastELement = smsArray[lastIndex];
-    const newSms = `${lastELement} ${word}`.trim();
-
-    if(newSms.length > maxSmsLength) {
-      return [...smsArray, word];
-    }
-
-    smsArray[lastIndex] = newSms;
-    return [...smsArray];
   }
