0207b42867ff6e280f5029638f441330a0dee172
Ihor Andriiv <andriivihor@icloud.com>
2019-12-23 21:57:18 +0200

    add intersection of 2 lists

---
+++ Data Structure/LinkedList/IntersectionOf2Lists-160/intersection.js
@@ -0,0 +1,46 @@
+// Solution based on Lists size
+// Time O(n or m) depends on A or B lists size
+// Space O(1)
+
+function getIntersectionNode (headA, headB) {
+    let countA = 0, countB = 0;
+    let currA = headA, currB = headB;
+       
+    if (!(headA && headB)) return null;
+    
+    while (currA || currB) {
+         
+        if (currA) {
+            countA++;
+            currA = currA.next;
+         }
+         
+        if (currB) {
+            countB++;
+            currB = currB.next;
+         }
+     }
+     
+     currA = headA, currB = headB;  
+       
+     while (countA !== countB) {
+         if(countA > countB) {
+             countA--;
+             currA = currA.next;
+          } else {
+            countB--; 
+            currB = currB.next;           
+          }
+     }
+             
+     while (currA || currB) {
+         if (currA === currB) {
+             return currA;
+         }
+         
+         currA = currA.next;
+         currB = currB.next;
+     }  
+       
+     return null;  
+   };
