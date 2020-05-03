d7de23c251d391444a7a0b54418e00bc4eb8514f
rihovardini <andriivihor@icloud.com>
2020-05-03 15:14:25 +0300

    add max depth n-ary tree problem

---
+++ Tree/559. Maximum Depth of N-ary Tree/depth.js
@@ -0,0 +1,40 @@
+// Time and Space Complexity  - O(n)
+
+// Iterative solution using queue;
+var maxDepth1 = function(root) {
+  if (root === null) return 0;
+  
+  if (root.children.length === 0) return 1;
+  
+  let depth = 0;
+  
+  const queue = [root];
+  
+  while(queue.length) {
+      const { length } = queue;
+
+      for (let i = 0; i < length; i++) {
+          const node = queue.shift();
+          
+          for (const child of node.children) {
+              queue.push(child);
+          }
+      }
+      depth++;
+  }
+  
+  return depth;
+};
+
+// Recursive solution
+var maxDepth2 = function(root) {
+  if (root === null) return 0;
+  
+  if (root.children.length === 0) return 1;
+  
+  let max = 0;
+  
+  for(const child of root.children) {
+      max = Math.max(maxDepth(child), max);
+  }
+  
