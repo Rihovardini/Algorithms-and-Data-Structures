3352bf237d55554e2e597c0317f74e3ff8c3f014
rihovardini <andriivihor@icloud.com>
2020-05-05 11:30:00 +0300

    add search problem

---
+++ Tree/700. Search in a Binary Search Tree/search.js
@@ -0,0 +1,11 @@
+// Time complexity - O (log n);
+// Space complexity - O (1)
+
+var searchBST = function(root, val) {
+  while (root !== null && root.val !== val) {
+      const isLess = root.val > val;
+      root = isLess ? root.left : root.right;
+  }
+  
+  return root;
+}
