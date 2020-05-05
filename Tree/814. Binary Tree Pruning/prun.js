79c556a1674657efdb269f7b91ed19e7cea85521
rihovardini <andriivihor@icloud.com>
2020-05-05 15:11:31 +0300

    add prunnig problem

---
+++ Tree/814. Binary Tree Pruning/prun.js
@@ -0,0 +1,14 @@
+// Time and Space complexity O (n)
+
+var pruneTree = function(root) {
+  if (root === null) return null;
+  
+  root.left = pruneTree(root.left);
+  root.right = pruneTree(root.right);
+  
+  if (root.val === 0 && root.left === null && root.right === null) {
+      return null;
+  }
+  
+  return root;
+};
